d65b7687904ead16ad9c9ed4a1be8323d1ec9dd6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-28 06:35:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,6 @@ class ApplicationController < Sinatra::Base
     if params[:username] == "" || params[:password] == ""
 		  redirect "/failure"
 		else
-		  # User.create(username: params[:username], password: params[:password])
 		  redirect "/login"
 		end
 
