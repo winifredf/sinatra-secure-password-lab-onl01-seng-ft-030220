ac5f1de6fdcc208e2943c003e6e5b36dd629c6a0
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-28 06:36:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,12 +36,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
+    user = User.find_by(:username: params[:username])
 		
-		if user &&
-		  user.authenticate(params[:password])
+		if user && user.authenticate(params[:password])
 		  session[:user_id] = user.id
-		  redirect "/success"
+		  redirect "/account"
 		else
 		  redirect "/failure"
 		end
